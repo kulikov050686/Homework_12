e2e6fe685bc4bb65293001f8e0732b20456eccf4
Павел Павел <kulikov050686@rambler.ru>
2021-05-10 22:42:16 +0300

    Установлена главная Модель-Представление через локатор

---
+++ Homework_12/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,13 @@
+﻿using Homework_12;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ViewModels
+{
+    /// <summary>
+    /// Класс для доступа к конкретным Моделям-Представления
+    /// </summary>
+    public class ViewModelLocator
+    {
+        public MainWindowViewModel MainWindowVM => App.Host.Services.GetRequiredService<MainWindowViewModel>();
+    }
+}
