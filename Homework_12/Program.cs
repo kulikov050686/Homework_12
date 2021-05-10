ec404826e70608cb9cf7a30691ef828826127b93
Павел Павел <kulikov050686@rambler.ru>
2021-05-10 21:53:57 +0300

    Создана собственная точка входа в приложение

---
+++ Homework_12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_12
+{
+    public static class Program
+    {
+        /// <summary>
+        /// Точка входа в приложение
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
