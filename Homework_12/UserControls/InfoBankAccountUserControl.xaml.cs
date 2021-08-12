6345b78dc6b6a2b5f33cf81097f88a0f5b010e3e
Павел Павел <kulikov050686@rambler.ru>
2021-08-12 16:07:40 +0300

    Рефакторинг ода

---
+++ Homework_12/UserControls/InfoBankAccountUserControl.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace UserControls
+{    
+    public partial class InfoBankAccountUserControl : UserControl
+    {        
+        public InfoBankAccountUserControl() => InitializeComponent();        
+    }
+}
--- Homework_12/UserControls/InfoBankCustomerUserControl.xaml.cs
+++ Homework_12/UserControls/InfoBankCustomerUserControl.xaml.cs
@@ -1,29 +1,9 @@
 ﻿using System.Windows.Controls;
-using System.ComponentModel;
-using System.Windows;
-using Models;
 
 namespace UserControls
 {    
     public partial class InfoBankCustomerUserControl : UserControl
     {
-        #region Контекст данных
-
-        public static new readonly DependencyProperty DataContextProperty =
-            DependencyProperty.Register(nameof(DataContext),
-                                        typeof(BankCustomer),
-                                        typeof(PersonUserControl),
-                                        new PropertyMetadata(default(BankCustomer)));
-
-        [Description("Контекст данных")]
-        public new BankCustomer DataContext
-        {
-            get => (BankCustomer)GetValue(DataContextProperty);
-            set => SetValue(DataContextProperty, value);
-        }
-
-        #endregion
-
         public InfoBankCustomerUserControl() => InitializeComponent();        
     }
 }
